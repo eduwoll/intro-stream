582e9c04827e60488098759838c0512d6295861d
Eduardo Woll <eduwoll007@gmail.com>
2023-01-12 11:13:54 -0300

    Rodapé centralizado

--- components/Rodape.tsx
+++ components/Rodape.tsx
@@ -2,25 +2,20 @@ import Logo from "./Logo";
 
 const Rodape: React.FC = () => (
   <>
-    <div
-      className="fixed bottom-0 left-0 w-screen bg-default"
-      style={{ height: 90 }}
-    />
-
-    <div
-      id="inicio"
-      className="fixed bottom-5 left-5 py-0 italic"
-      style={{
-        fontSize: "2em",
-      }}
-    >
-      Começando em breve...
-    </div>
-
-    <div className="fixed bottom-1 right-5">
-      <div className="flex justify-center items-center">
-        <div className="mr-1 font-[Merienda]">Eu Creio</div>
+    <div className="fixed bottom-0 left-0 w-screen bg-default flex justify-center items-center p-2">
+      <div className="flex items-center">
         <Logo width={80} height={80} />
+        <div className="mr-1 text-2xl font-[Merienda]">Eu Creio</div>
+      </div>
+      <div className="w-10" />
+      <div
+        id="inicio"
+        className="italic"
+        style={{
+          fontSize: "2em",
+        }}
+      >
+        Começando em breve...
       </div>
     </div>
   </>
